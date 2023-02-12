69284a45b33b52807630c3c61feb9acc19725979
Mahran <116034942+1MahMah5@users.noreply.github.com>
2023-02-12 20:32:01 +0100

    Update CalculatorScript.js

--- task4/CalculatorScript.js
+++ task4/CalculatorScript.js
@@ -1,9 +1,16 @@
 function erase(){
-    document.getElementById("result").value="";
+    document.getElementById("result").value=''
 }
 
 function disp(x){
-   document.getElementById("result").value+=x;
+    op = '/*-+.'
+    res = document.getElementById("result").value
+    if ((op.indexOf(x) != -1) && ((op.indexOf(res[res.length-1]) != -1) || (res == ''))) {
+        alert('Typing error ! \nWrong character deleted !')
+    }
+    else {
+        document.getElementById("result").value += x
+    }
 }
 
 function equal(){
